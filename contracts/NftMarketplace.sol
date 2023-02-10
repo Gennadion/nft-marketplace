86f2256c93978dc91f703b3f88766259817e1e10
Gennadion <ghammurti@gmail.com>
2023-02-10 18:37:10 +0200

    NftMarketplace contract

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+
+contract NftMarketplace {
+	function listItem(address nftAddress, uint256 tokenId, uint256 price) external {
+        if(price <= 0){
+            revert NftMarketplace__PriceMustBeAboveZero();
+        }
+
+        IERC721 nft = IERC721(nftAddress);
+        if(nft.getApproved(tokenId) != address(this)){
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+    }
+}
