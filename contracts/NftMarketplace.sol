8ef816ddf1cdc5b8b1cbe1e7db458ccb27184a01
Gennadion <ghammurti@gmail.com>
2023-02-10 18:53:48 +0200

    Added modifiers and some error handlers

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -5,16 +5,51 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
 
 contract NftMarketplace {
-	function listItem(address nftAddress, uint256 tokenId, uint256 price) external {
-        if(price <= 0){
-            revert NftMarketplace__PriceMustBeAboveZero();
-        }
+	struct Listing {
+		uint256 price;
+		address seller;
+	}
 
-        IERC721 nft = IERC721(nftAddress);
-        if(nft.getApproved(tokenId) != address(this)){
-            revert NftMarketplace__NotApprovedForMarketplace();
-        }
-    }
+	event ItemListed(
+		address indexed seller,
+		address indexed nftAddress,
+		uint256 indexed tokenId,
+		uint256 price
+	);
+
+	mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+	// Modifiers
+	modifier notListed(
+		address nftAddress,
+		uint256 tokenId,
+		address owner
+	) {
+		Listing memory listing = s_listings[nftAddress][tokenId];
+		if (listing.price > 0) {
+			revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+		}
+		_;
+	}
+
+	// Main Functions
+	function listItem(
+		address nftAddress,
+		uint256 tokenId,
+		uint256 price
+	) external notListed(nftAddress, tokenId, msg.sender) {
+		if (price <= 0) {
+			revert NftMarketplace__PriceMustBeAboveZero();
+		}
+
+		IERC721 nft = IERC721(nftAddress);
+		if (nft.getApproved(tokenId) != address(this)) {
+			revert NftMarketplace__NotApprovedForMarketplace();
+		}
+		s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+		emit ItemListed(msg.sender, nftAddress, tokenId, price);
+	}
 }
