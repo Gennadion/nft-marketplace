38397b23e4227a90df4c63a1a0ec5e66ca3da402
Gennadion <ghammurti@gmail.com>
2023-02-12 14:18:00 +0200

    Getter view functions

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -154,4 +154,17 @@ contract NftMarketplace is ReentrancyGuard {
 			revert NftMarketplace__TransferFailed();
 		}
 	}
+
+	// Getter Functions
+
+	function getListing(
+		address nftAddress,
+		uint256 tokenId
+	) external view returns (Listing memory) {
+		return s_listings[nftAddress][tokenId];
+	}
+
+	function getProceeds(address seller) external view returns(uint256){
+		return s_proceeds[seller];
+	}
 }
